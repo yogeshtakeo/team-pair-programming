b7bf399c425b984019b07efe267ebd3ec4826920
babisha <mdr.bebo@gmail.com>
2023-06-14 01:28:38 -0400

    rendering through props

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,18 @@
+
+
+
+function Button(props) {
+
+    
+    return (
+    <>
+        <button className= "p-5 bg-green-300">{props.children}</button>
+        
+
+    </>
+    )
+    }
+
+export default Button
+
+
