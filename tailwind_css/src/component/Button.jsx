c9c435a1e4e8e7814cf08064c0d6669d17a50b91
Ashma Upreti <ashma.upreti99@gmail.com>
2023-06-16 20:56:54 -0400

    task 16: primary and secondary component created

--- tailwind_css/src/component/Button.jsx
+++ tailwind_css/src/component/Button.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 
 function Button(props) {
-  console.log(props);
-  return <button className="bg-orange-400 w-52 ">{props.children}</button>;
+  const { primary, secondary } = props;
+  console.log(typeof props.disable);
+  // const disable = true;
+  if (primary)
+    return (
+      <button disabled={props.disable} className="bg-orange-400 w-52 rounded ">
+        {props.children}
+      </button>
+    );
+  else secondary;
+  return (
+    <button disabled={props.disable} className="bg-orange-400 w-32 rounded ">
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
--- tailwind_css/src/component/Form.jsx
+++ tailwind_css/src/component/Form.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Button from "./Button";
 
 function Form(props) {
   return (
-    <div>
-      <input type="text" placeholder="Enter your email" />
+    <div className=" flex  justify-center items-center p-10 ">
+      <input
+        className="border-2 border-amber-500 rounded mr-5"
+        type="text"
+        placeholder="Enter your shop item"
+      />
+      <Button secondary disable={false}>
+        Add to Card
+      </Button>
     </div>
   );
 }
