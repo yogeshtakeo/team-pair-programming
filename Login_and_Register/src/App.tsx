b2ccbc7a32d39c81d4997e2220c1020481257d07
Kabina Thapa <thapakabina28@gmail.com>
2023-06-22 16:55:04 -0400

    Add LoginPage Component

--- Login_and_Register/src/App.tsx
+++ Login_and_Register/src/App.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 
 import './App.css'
+import LoginPage from './Components/LoginPage'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <LoginPage/>
     </>
   )
 }
--- Login_and_Register/src/Components/LoginPage.tsx
+++ Login_and_Register/src/Components/LoginPage.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { ChangeEvent, FormEvent } from 'react';
 
 const LoginPage = () => {
+    const[email, setEmail]=useState('')
+    const[password, setPassword]=useState('')
+    const[submit, setSubmit]=useState('')
+    const handleEmail=(event:ChangeEvent<HTMLInputElement>)=>{
+        setEmail(event.target.value)
+     }
+    const handlePassword=(event:ChangeEvent<HTMLInputElement>)=>{
+        setPassword(event.target.value)
+    }
+    const handleSubmit=(event:FormEvent<HTMLFormElement>)=>{
+        event.preventDefault()
+        const formData={
+            email,
+            password,
+        }
+        setSubmit(formData)
+        setEmail('')
+        setPassword('')
+        
+    }
+
+    
   return (
   <>
-  <div>
-        <form>
-           <label></label>
-
-        </form>
+  <div className='flex flex-col w-1/2 justify-center p-24 h-screen mx-auto bg-stone-300 m-12'>
+    <h1 className='text-center text-2xl font-semibold mb-8'>Log In</h1>
+        <form className='flex flex-col' onSubmit={handleSubmit}>
+           <label className='text-lg' htmlFor="email">Username</label>
+           <input className='border-2 border-black rounded-md p-2 mb-8' type="email" id="email" value={email} onChange={handleEmail}/>
+           <label className='text-lg' htmlFor='password'>Password</label>
+           <input  className='border-2 border-black rounded-md p-2' type="password" id="password" value={password} onChange={handlePassword}/>
+           </form>
+           <button className='border-2 border-black mt-8 rounded-md w-32 mx-auto p-1' type="submit">Log In</button>
     </div>
     </>
   )
