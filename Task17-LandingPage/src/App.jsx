608c02a9ff8ebbb3692929bdd5ea30b1175cda84
JunuThapa <junuthapa011@gmail.com>
2023-06-22 02:18:11 -0400

    Task17: changed footer component

--- Task17-LandingPage/src/App.jsx
+++ Task17-LandingPage/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   
   return(
   <>
+  {/* <div className='flex sm:justify-around space-x-12 bg-blue-200 py-4'> */}
   <div className="flex justify-around space-x-4 bg-blue-200 py-5">
     <Navbar className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</Navbar>
     <Navbar className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Services</Navbar>
@@ -19,6 +20,9 @@ function App() {
     <Navbar className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Sign-up</Navbar>
   </div>
 
+  {/* <div>
+    <Footer className="relative bottom-0"/>
+  </div> */}
     </>
   )
   
