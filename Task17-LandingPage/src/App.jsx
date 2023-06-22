c0348b1243746afb94cc70c00bdf65e285fa8993
JunuThapa <junuthapa011@gmail.com>
2023-06-22 01:14:30 -0400

    Task17: changed navbar styling

--- Task17-LandingPage/src/App.jsx
+++ Task17-LandingPage/src/App.jsx
@@ -5,16 +5,14 @@ function App() {
   
   return(
   <>
-  <div>
-    <Navbar>Home</Navbar>
-    <Navbar>Services</Navbar>
-    <Navbar>Blogs</Navbar>
-    <Navbar>Login</Navbar>
-    <Navbar>Sign-up</Navbar>
+  <div className="flex justify-around space-x-4 bg-blue-200 py-5">
+    <Navbar className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</Navbar>
+    <Navbar className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Services</Navbar>
+    <Navbar className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Blogs</Navbar>
+    <Navbar className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Login</Navbar>
+    <Navbar className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Sign-up</Navbar>
   </div>
 
-
-    {/* <button className='bg-black-200'>click</button> */}
     </>
   )
   
--- Task17-LandingPage/src/Components/Navbar.jsx
+++ Task17-LandingPage/src/Components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Navbar(props) {
   return (
-    <div className='flex flex-col bg-blue-200'>
+    <div className="flex justify-center space-x-4">
         <nav>
-            <a href = "">{props.children}</a>
+            <a href = "" className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">{props.children}</a>
         </nav>
     </div>
   )
