7aa89dde9cd403fe60840e5b6ffbe3cf7c46296a
JunuThapa <junuthapa011@gmail.com>
2023-06-23 02:22:03 -0400

    Task 17: added Login form inside Modal component

--- Task17-LandingPage/src/App.jsx
+++ Task17-LandingPage/src/App.jsx
@@ -16,9 +16,8 @@ function App() {
 
   return(
     <>
+      <div className='flex flex-row items-center bg-indigo-100 py-2'>
 
-      <div className='flex flex-row items-center bg-rose-100 py-2'>
-        
         <a href="Logo" className='grid grid-cols-2 items-end relative left-10'>
           <img src="https://www.logolynx.com/images/logolynx/ab/ab3cf43cb423c7d9c20eadde6a051a5d.jpeg" className= "h-8 m-2" alt="CUNY Logo" />
           <span className=" self-center font-large py-2 whitespace-nowrap ">C.U.N.Y</span>
@@ -29,15 +28,18 @@ function App() {
           <Navbar>ACADEMICS</Navbar>
           <Navbar>FINANCIAL AID</Navbar>
           <Navbar>ADMISSIONS</Navbar>
-          <Navbar onClick={() => {setIsClick(true)}}>APPLY</Navbar>
-        </div>
-        
+          <Navbar onClick={() => {setIsClick(true)}}>LOGIN</Navbar>
+        </div> 
+
       </div>
   
       <div>
         <Footer/>
       </div> 
-      {isClick && <Modal  onClick={() => {setIsClick(false)}}/>}
+      
+      <div className='flex justify-center'>
+        {isClick && <Modal onClick={() => {setIsClick(false)}}/>}
+      </div>
     </>
   )
   
--- Task17-LandingPage/src/Components/Modal.jsx
+++ Task17-LandingPage/src/Components/Modal.jsx
@@ -8,19 +8,26 @@ function Modal(props) {
 return (
   <>
   
-    <div className='inline-block bg-gray-200 border-solid border-gray-500 border-2 m-10 p-5'>
+    <div className='flex flex-col items-center justify-center bg-gray-300 border-solid border-gray-500 border-2 m-10 p-5 rounded-xl w-1/4 h-100'>
       
-        <div className= "p-2">
-          <h4 className='modal-title'>
-            This is a Modal
-          </h4>
-        </div>
-        <div className= "p-2">
-          I hope you got in!
+        <div className= "flex flex-col">
+          <form>
+            <p className='relative text-xl mb-4 '>LOGIN</p>
+            
+            <p className=' text-sm '>Username</p>
+            <input type="textbox" placeholder='email@address.com' className=' rounded p-0.5'></input>
 
-        </div>
-        <div className= "p-2">
-          <button onClick={props.onClick}>CLOSE</button>
+            <p className=' text-sm '>Password</p>
+            <input type="password" placeholder='Password' className=' rounded p-0.5'></input>
+            
+            <div className='flex flex-row'>
+              <input type="checkbox"></input>
+              <p>Remember me</p>
+            </div>
+          </form>
+          
+          <button onClick={props.onClick} className=' text-sm text-white bg-blue-900 border-solid border-gray-800 hover:bg-slate-100 hover:text-slate-900 border-2 rounded p-1 mt-2 w-1/3'>Login</button>
+         
         </div>    
     </div>
   </>
