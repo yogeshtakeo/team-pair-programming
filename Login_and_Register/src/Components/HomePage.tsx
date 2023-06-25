8e5da25b54df3409f282a3f454555d2d88dbe0d8
Kabina Thapa <thapakabina28@gmail.com>
2023-06-24 22:45:49 -0400

    Add Animation glow

--- Login_and_Register/src/Components/HomePage.tsx
+++ Login_and_Register/src/Components/HomePage.tsx
@@ -4,9 +4,9 @@ import {AiOutlineDownCircle} from 'react-icons/ai'
 const HomePage = () => {
   
   return (
-    <div className='outline  w-full h-screen' >
+    <div className='outline w-full h-screen bg-gradient-to-t from-blue-800 to-blue-900' >
 
-    <div className='flex flex-col justify-center items-center w-full h-screen bg-yellow bg-opacity-50'>
+    <div className=' outline1 flex flex-col justify-center items-center w-full h-screen bg-opacity-50'>
       <div className='b absolute right-[30%] bottom-60 flex flex-col items-center'>
       <div className='w-24 h-24 rounded-full bg-emerald-500 shadow-xl shadow-emerald-600'></div>
       <div className=' h-32 border-2 border-l-0 border-slate-200 mb-4'></div>
@@ -15,11 +15,11 @@ const HomePage = () => {
          <div className='w-24 h-24 rounded-full bg-red-500 shadow-xl shadow-red-400'></div>
        <div className=' h-32 border-2 border-l-0 border-slate-200 mb-4'></div>
       </div>
-      <div className='b2 absolute right-[50%] bottom- flex flex-col items-center' >
+      <div className='b2 absolute right-[65%] bottom- flex flex-col items-center' >
        <div className='w-24 h-24 rounded-full bg-yellow-500 shadow-xl shadow-yellow-600'></div>
        <div className=' h-32 border-2 border-l-0 border-slate-200 mb-4'></div>
       </div>
-      <div className=' b3 absolute right-[39%] bottom-72 flex flex-col items-center' >
+      <div className=' b3 absolute right-[48%] bottom-72 flex flex-col items-center' >
        <div className='w-24 h-24 rounded-full shadow-xl shadow-purple-600 bg-purple-500'></div>
        <div className=' h-32 border-2 border-l-0 border-slate-200 mb-4'></div>
       </div>
@@ -28,7 +28,7 @@ const HomePage = () => {
        <div className=' h-32 border-2 border-l-0 border-slate-200 mb-4'></div>
       </div>
       <h1 className='text-white text-4xl font-semibold'>Welcome!</h1>
-      <button className='text-white  text-3xl mt-4 rounded-full'>Let's Get Started <AiOutlineDownCircle className='m-auto mt-4 hover:scale-110' size={45}/> </button>
+      <button className='button text-white  text-3xl '>Let's Get Started <AiOutlineDownCircle className='glow m-auto mt-4 hover:scale-110 rounded-full' size={45}/> </button>
     </div>
     </div>
   )
