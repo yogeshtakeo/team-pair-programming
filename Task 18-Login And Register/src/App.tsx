461760830f6152aba4b2c81bc8a8bbfaa85dfc58
babisha <mdr.bebo@gmail.com>
2023-07-04 00:57:10 -0400

    Navigation added to pages

--- Task 18-Login And Register/src/App.tsx
+++ Task 18-Login And Register/src/App.tsx
@@ -3,7 +3,7 @@ import RegisterPage from './components/Pages/Register'
 import HomePage from './components/Pages/Home'
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
-import React from 'react'
+
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='"LoginPage' element={<LoginPage/>}/>
+        <Route path='LoginPage' element={<LoginPage/>}/>
         <Route path='HomePage' element={<HomePage/>}/>
         <Route path='RegisterPage' element={<RegisterPage/>}/>
       </Routes>
--- Task 18-Login And Register/src/components/Pages/Login.tsx
+++ Task 18-Login And Register/src/components/Pages/Login.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import {useLocation, useNavigate} from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
+
 const LoginPage = () => {
     const[username, setUsername] = useState('')
     const[password, setPassword] = useState('')
@@ -9,17 +10,9 @@ const LoginPage = () => {
     const navigate = useNavigate()
     const location = useLocation()
 
+    function handleSubmit() {
 
-    const handleUsername = (event) =>{
-        (event =>setUsername(event.target.value))
-    }
-
-    const handlePassword = (event) => {
-        (event =>setPassword(event.target.value))
-    }
-
-    const handleSubmit = (event) =>{
-        event.preventDefault()
+        // event.preventDefault()
         const confirmPassword = location?.state?.confirmPassword 
         if(password === confirmPassword){
             navigate('/Home')
@@ -30,11 +23,11 @@ const LoginPage = () => {
         <div>
            <form>
             <label htmlFor="email">EMAIL</label>
-              <input type="email" value={username} placeholder="Full Name" onChange={handleUsername} />
+              <input type="email" value={username} placeholder="Full Name" onChange={(event)=>setUsername(event.target.value)} />
               <label htmlFor="password">PASSWORD</label>
-              <input type="Password" value={password} placeholder="Enter Your Password" onChange={handlePassword}/>
+              <input type="Password" value={password} placeholder="Enter Your Password" onChange={(event)=>setPassword(event.target.value)}/>
               <p><input type="checkbox"/>REMEMBER ME...</p>
-              <button type="submit">LOG IN</button>
+              <button type="submit" onClick={handleSubmit}>LOG IN</button>
 
               <p>FORGOT PASSWORD?</p>
               <p>REGISTER FOR NEW ACCOUNT<Link to ='/RegisterPage'>SIGN UP</Link></p>
